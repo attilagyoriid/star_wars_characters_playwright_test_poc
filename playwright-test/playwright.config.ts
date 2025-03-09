aac315d3928c8c69217aeabfd2bb53a16347e87e
attilagyoriid <attila.gyori.id@gmail.com>
2025-03-09 15:50:57 +0100

    no need for webserver

--- playwright-test/playwright.config.ts
+++ playwright-test/playwright.config.ts
@@ -77,11 +77,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
-    cwd: '../',
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: 'npm run dev',
+  //   url: 'http://localhost:3000',
+  //   cwd: '../',
+  //   timeout: 120 * 1000,
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
